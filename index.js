9b023ea83be7b7fc1db4974aa8f72e2a2e43cf84
xi zhao <sissi.zhao09@gmail.com>
2025-11-17 19:22:07 -0500

    change config back

--- index.js
+++ index.js
@@ -26,15 +26,6 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
-if (process.env.SERVER_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-    domain: process.env.SERVER_URL,
-  };
-}
-
 // if (process.env.SERVER_ENV !== "development") {
 //   sessionOptions.proxy = true;
 //   sessionOptions.cookie = {
@@ -42,13 +33,22 @@ if (process.env.SERVER_ENV !== "development") {
 //     secure: true,
 //     // domain: process.env.SERVER_URL,
 //   };
-// } else {
-//   // local development: http://localhost:4000
-//   sessionOptions.cookie = {
-//     sameSite: "lax",
-//     secure: false,
-//   };
 // }
+
+if (process.env.SERVER_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    // domain: process.env.SERVER_URL,
+  };
+} else {
+  // local development: http://localhost:4000
+  sessionOptions.cookie = {
+    sameSite: "lax",
+    secure: false,
+  };
+}
 
 app.use(session(sessionOptions));
 
