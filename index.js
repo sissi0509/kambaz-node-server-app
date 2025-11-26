abd4b2c77545aa31d803c042c41a202c79108757
xi zhao <sissi.zhao09@gmail.com>
2025-11-25 21:32:45 -0500

    triger vercel build

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ app.use(
     origin: process.env.CLIENT_URL || "http://localhost:3000",
   })
 );
-// app.set("trust proxy", 1);
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
